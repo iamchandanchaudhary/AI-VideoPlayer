e2c097bbdd436296107ae06e3a4d3b7ee1e14f7b
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2026-06-17 17:23:14 +0530

    database connection

--- models/User.ts
+++ models/User.ts
@@ -23,7 +23,7 @@ userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 10);
     }
-    next();
+    // next();
 });
 
 const User = models?.User || model<IUser>("User", userSchema);
